3eb2a97195bc8f4938e8650f76a0412d4c1b7e29
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2023-08-02 13:15:43 -0400

    6

--- r-requirements/install.R
+++ r-requirements/install.R
@@ -1,11 +1,15 @@
 #!/usr/bin/env Rscript
 cat("Installing R dependencies...\n")
 
-cran_packages <- c("magrittr")
+cran_packages <- c(
+    "magrittr", "tibble", "plyr", "feather", "dplyr",
+    "RSQLite", "DBI", "WGCNA", "ashr", "corpcor", "tidyverse")
 all_packages <- c(cran_packages)
 
 install.packages(all_packages)
 
 packages_installed <- all_packages %in% installed.packages()
+print(packages.installed)
+
 stopifnot(all(packages_installed))
 cat("Confirmed that all packages were successfully installed\n")
