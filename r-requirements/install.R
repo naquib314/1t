35348f3a6a1c0b63c3155c2178ca65bb8c05eb48
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2023-08-02 12:55:34 -0400

    2

--- r-requirements/install.R
+++ r-requirements/install.R
@@ -2,23 +2,11 @@
 cat("Installing R dependencies...\n")
 
 cran_packages <- c("magrittr")
-bioconductor_packages <- c("impute")
-all_packages <- c(cran_packages, bioconductor_packages)
+all_packages <- c(cran_packages)
 
 packages_installed <- all_packages %in% installed_packages()
 
-if(!all(packages_installed)) {
-    install.packages(cran_packages, repos = "http://cran.us.r-project.org")
-
-    if (!requireNamespace("BiocManager", quietly = TRUE))
-       install.packages("BiocManager", 
-       repos = "http://bioconductor.org/packages/3.9/bioc")
-
-
-    for (package in bioconductor_packages) {
-        BiocManager::install(package)
-    }
-}
+install.packages("magrittr")
 
 packages_installed <- all_packages %in% installed_packages()
 stopifnot(all(packages_installed))
