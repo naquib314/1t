c388624b2dd5ed6cbf1c4d6648fc6cf059eb8883
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2023-08-02 13:04:24 -0400

    5

--- r-requirements/install.R
+++ r-requirements/install.R
@@ -6,6 +6,6 @@ all_packages <- c(cran_packages)
 
 install.packages(all_packages)
 
-packages_installed <- all_packages %in% installed_packages()
+packages_installed <- all_packages %in% installed.packages()
 stopifnot(all(packages_installed))
 cat("Confirmed that all packages were successfully installed\n")
