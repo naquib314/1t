b2b7fc5450b7a542a47c2ef726f795b9a0b34965
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2023-08-07 12:36:45 -0400

    a

--- app1/build.sh
+++ app1/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 DOCKERFILE_DIR="$1"
-
-CONSEQ_VERSION=2.0.2
-if [ ! -e conseq-${CONSEQ_VERSION}.tar.gz ] ; then
-  curl -L https://github.com/broadinstitute/conseq/releases/download/v${CONSEQ_VERSION}/conseq-${CONSEQ_VERSION}.tar.gz -o $(DOCKERFILE_DIR)/conseq-${CONSEQ_VERSION}.tar.gz
-fi
-
 echo ls -l $(DOCKERFILE_DIR)
+
+# CONSEQ_VERSION=2.0.2
+# if [ ! -e conseq-${CONSEQ_VERSION}.tar.gz ] ; then
+#   curl -L https://github.com/broadinstitute/conseq/releases/download/v${CONSEQ_VERSION}/conseq-${CONSEQ_VERSION}.tar.gz -o $(DOCKERFILE_DIR)/conseq-${CONSEQ_VERSION}.tar.gz
+# fi
+
+# echo ls -l $(DOCKERFILE_DIR)
